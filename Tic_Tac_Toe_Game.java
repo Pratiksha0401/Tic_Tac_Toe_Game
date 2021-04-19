45d8a351be10da5048f3f72ab55be6adf84ff488
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-19 14:47:24 +0530

    Creating a tic tac toe board

--- Tic_Tac_Toe_Game.java
+++ Tic_Tac_Toe_Game.java
@@ -1,9 +1,24 @@
-class Tic_Tac_Toe_Game 
+class Tic_Tac_Toe_Game implements Board
 {
+
+	public void UC1(){
+	
+		char [] board={' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+		for(int i=1;i<board.length;i++)
+		{
+			System.out.println(board[i]+"|"+board[(++i)]+"|"+board[(++i)]);
+			System.out.println("-+"+"-+-");
+
+		}
+	}
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to Tic Tac Toe Game");
+		Tic_Tac_Toe_Game t1=new Tic_Tac_Toe_Game();
+		t1.UC1();
 	}
 }
 
-
+interface Board
+{
+	void UC1();
+}

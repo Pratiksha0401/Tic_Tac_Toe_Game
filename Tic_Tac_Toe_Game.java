81bf80c7a526c26f266827830f2b3faa2db6657e
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-19 14:43:40 +0530

    Welcom to Tic Tac Toe game

--- Tic_Tac_Toe_Game.java
+++ Tic_Tac_Toe_Game.java
@@ -1,25 +1,9 @@
-class Tic_Tac_Toe_Game implements Board
+class Tic_Tac_Toe_Game 
 {
-
-	public void UC1(){
-	
-		char [] board={' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
-		for(int i=1;i<board.length;i++)
-		{
-			System.out.println(board[i]+"|"+board[(++i)]+"|"+board[(++i)]);
-			System.out.println("-+"+"-+-");
-
-		}
-	}
 	public static void main(String[] args) 
 	{
-		Tic_Tac_Toe_Game t1=new Tic_Tac_Toe_Game();
-		t1.UC1();
+		System.out.println("Welcome to Tic Tac Toe Game");
 	}
 }
 
-interface Board
-{
-	void UC1();
-}
 
